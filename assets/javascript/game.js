c6f1d7c8cb7a6930ecef15b0aa558047c6d93571
Tyler <tgmilka@gmail.com>
2016-10-26 22:20:40 -0500

    asdf

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+$(document).ready.(function() {}
+
+
+redGem: function ()
+
+	$("redGem").click(function);{
+
+	}
+
+	//4 functions; each for individual gems
+
+
+	//random number generater 
+
+	//when clicked, each crystal will generate and display a
+	//unique number added to the total score display box 
+
