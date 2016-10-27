9cc6a7bcebeceec3f787e90d4eca254c642e40a8
Tyler <tgmilka@gmail.com>
2016-10-27 17:25:29 -0500

    Started Java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,54 @@
-$(document).ready.(function() {}
+//declares that game is able to start as soon as its loaded
+$(document).ready(function() {
+
+	
+	var $randomNumber = $("#randomNumber");
+//generate random number at the start of the game
+$(document).ready(function(){
+	var $randomNumber=Math.floor(Math.random() * 40) + 1;
+	$('#result').html($randomNumber); 
+	return false;
+	});
+
+//generate a number for each gem that when pressed gets 
+//added to the total score variable
+
+	var $redGem = $("#redGem");
+	var $blueGem = $("#blueGem");
+	var $yellowGem =$("#yellowGem");
+	var $greenGem =$("#greenGem");
+
+$(document).ready(function() {
+	$("#redGem").mousedown(function() {
+		$("#redGem").height(+200);
+		if 
+	});
+		$("#redGem").mouseup(function(){
+		$("#redGem").height(+100);
+
+	});
+	});
+	});
+
+$(document).ready(function() {
+	$("#blueGem").mousedown(function() {
+		$("#blueGem").height(+200);
+	});
+		$("#blueGem").mouseup(function(){
+		$("#blueGem").height(+100);
+
+	});
+	});
+	
 
 
-redGem: function ()
 
-	$("redGem").click(function);{
 
-	}
+
+
+
+
+
 
 	//4 functions; each for individual gems
 
@@ -14,4 +57,6 @@ redGem: function ()
 
 	//when clicked, each crystal will generate and display a
 	//unique number added to the total score display box 
+
+
 
