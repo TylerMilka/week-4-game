99684bdaebeb3dc92a6e8dd77aaa978028fe92b1
Tyler <tgmilka@gmail.com>
2016-10-27 23:21:07 -0500

    safety

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 //declares that game is able to start as soon as its loaded
 $(document).ready(function() {
-
 	
 	var $randomNumber = $("#randomNumber");
+
 //generate random number at the start of the game
 $(document).ready(function(){
 	var $randomNumber=Math.floor(Math.random() * 40) + 1;
@@ -12,34 +12,41 @@ $(document).ready(function(){
 
 //generate a number for each gem that when pressed gets 
 //added to the total score variable
-
+	
+	var $totalScore =$("#score");
+	var score = 0;
 	var $redGem = $("#redGem");
 	var $blueGem = $("#blueGem");
 	var $yellowGem =$("#yellowGem");
 	var $greenGem =$("#greenGem");
-
+	
 $(document).ready(function() {
 	$("#redGem").mousedown(function() {
 		$("#redGem").height(+200);
-		if 
+		$score= (score + 10).html;
+
+
+
 	});
 		$("#redGem").mouseup(function(){
-		$("#redGem").height(+100);
-
-	});
+			$("#redGem").height(+100);
 	});
 	});
 
-$(document).ready(function() {
-	$("#blueGem").mousedown(function() {
-		$("#blueGem").height(+200);
-	});
-		$("#blueGem").mouseup(function(){
-		$("#blueGem").height(+100);
+////generate a number for each gem that when pressed gets 
+//added to the total score variable
+
+
+
+
+
+
+
+
 
 	});
-	});
-	
+
+
 
 
 
